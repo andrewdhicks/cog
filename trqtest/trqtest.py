5ff421b1fc9cbd64086e1cdd16e58d7b6146094c
Tony Butzer <tonybutzer@gmail.com>
2017-10-29 18:06:53 +0000

    added redis server to trqtest

--- trqtest/trqtest.py
+++ trqtest/trqtest.py
@@ -7,27 +7,22 @@ import argparse
 def bullshit():
     print("BULSHIT Walks!")
 
-def workerbee(task):
+def workerbee(redisServer, task):
     print("the TASK is %s" % task)
 
     queue = Queue(connection=Redis())
     queue.enqueue('time.sleep',5)
-    worker = SimpleWorker([queue], connection=queue.connection)
-    worker.work(burst=True)  # Runs enqueued job
     # Check for result...
     queue.enqueue(bullshit)
-    worker = SimpleWorker([queue], connection=queue.connection)
-    worker.work(burst=True)  # Runs enqueued job
 
 
 def mainsub():
-    #workerbee(task='builddir l7xxx')
     parser = argparse.ArgumentParser(description='Test RQ by sending some work python subs')
-    parser.add_argument('url', metavar='URL', type=string, nargs='+',
-                    help='the url for the redis server ')
+    parser.add_argument('url', help='the url for the redis server ')
 
     args = parser.parse_args()
     print (args.url)
+    workerbee(redisServer=args.url, task='builddir l7xxx')
 
 if __name__ == "__main__":
     mainsub()
